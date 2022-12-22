0b89d0eecb5b13c5e9da668dd240c241ed86d0fc
Aayush Shukla <aayushshukla@Aayushs-MacBook-Pro.local>
2022-12-21 23:35:10 -0800

    added portfolio code

---
+++ src/components/Skills.js
@@ -0,0 +1,40 @@
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
+import React from "react";
+import { skills } from "../data";
+
+export default function Skills() {
+  return (
+    <section id="skills">
+      <div className="container px-5 py-10 mx-auto">
+        <div className="text-center mb-20">
+          <ChipIcon className="w-10 inline-block mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+            Skills &amp; Technologies
+          </h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          I am a backend developer with a diverse skill set and experience working with a variety of technologies.
+          My primary expertise lies in Java and Spring Boot development, but I also have experience working with 
+          other frameworks such as Flask and technologies such as PostgreSQL. I have a strong foundation in JavaScript 
+          and have used it to build a wide range of applications. In addition, I have experience working with React on
+          the front-end and am proficient in modern front-end technologies such as Redux and Webpack. I also have a
+          strong foundation in Python and have used it to build a variety of applications, including web applications
+          and data processing scripts. Overall, I have a diverse set of skills that allow me to tackle a wide range of
+          projects and challenges.
+          </p>
+        </div>
+        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+          {skills.map((skill) => (
+            <div key={skill} className="p-2 sm:w-1/2 w-full">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-white">
+                  {skill}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Headcount Forecasting Tool",
+    subtitle: "Spring Boot and React",
+    description:
+    "The headcount forecasting web application is a tool that allows HR professionals to predict future staffing needs for their organization. It was developed using Spring Boot on the backend and React on the frontend.The application allows users to input data such as current headcount, planned hires and departures, and any known changes in business needs. The Spring Boot backend handles all data processing and storage, as well as providing a RESTful API for the React frontend to consume.",
+    image: "./project-1.gif",
+  },
+  {
+    title: "CoWIN Slot Notifier",
+    subtitle: "Spring Boot and React",
+    description:
+    "The CoWIN slot notification application is a tool that helps users secure appointments for the COVID-19 vaccine in India. It was developed using a combination of technologies, including Spring Boot on the backend and React on the frontend.The application allows users to input their location and vaccine preferences, and then sends notifications when slots become available at nearby vaccination centers. It also provides real-time updates on the availability of different vaccine brands and doses.",
+    image: "./project-2.gif",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+    "Aayush comes out as a well rounded, confident and comitted employee whom you would definitely want in your team. Ayush's technical skills are impressive and he showed time and again that he can adapt, learn and deliver what is needed within timelines with highest quality. He excelled in all the projects he was assigned to - be it Java, AWS, Jira workflows. His fluent commication is also a great asset and he took on the additional duties of coordination, demos and keeping all stakeholders appraised of the status. I see great potential in his abilities and wishing him the best for his future endeavours.",
+    image: "https://media.licdn.com/dms/image/C4D03AQEgiJpeOGAM2g/profile-displayphoto-shrink_100_100/0/1621349135710?e=1677110400&v=beta&t=iKX_w0F7C5bU0OYrw0eo723W6pcJyEgswFR-GXthyNE",
+    name: "Vishwanath Krishnan",
+    company: "Capgemini",
+  },
+  {
+    quote:
+    "Aayush worked in my team for 2 years as a full stack developer. He has exhibited exceptional technical skills during the tenure. Aayush's role was critical to the application being developed. He is dependable, Hard working and has outstanding technical skills as well as communication skills. Aayush would be an asset to have on any team. ",
+    image: "https://media.licdn.com/dms/image/C4E03AQHviGweZdpB5Q/profile-displayphoto-shrink_100_100/0/1632329790068?e=1677110400&v=beta&t=wVr36yt7jGAhBa17Z-b8yX2AwUMhRQ36PGj524XxraA",
+    name: "Neha Prasad",
+    company: "Capgemini",
+  },
+];
+
+export const skills = [
+  "Java",
+  "Spring",
+  "JavaScript",
+  "React",
+  "Python",
+  "Flask",
+  "PostgreSQL",
+  "SQL Server"
+];
